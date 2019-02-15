03770486f85f9ee2c542e18f1a0d986fd4e62d09
Andrius Cepaitis <andrius.c@sanitex.eu>
2019-02-15 13:02:31 +0200

    Add core.countries

--- matrix/core/admin.py
+++ matrix/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User, Contact
+from .models import User, Country, Contact
 
 admin.site.register(User, UserAdmin)
 # admin.site.unregister(auth.models.Group)
@@ -9,6 +9,10 @@ admin.site.register(User, UserAdmin)
 admin.site.site_header = "Matrix Admin"
 admin.site.site_title = "Matrix Admin Portal"
 admin.site.index_title = "Welcome to Matrix Researcher Portal"
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'alpha2', 'alpha3']
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
--- matrix/core/models.py
+++ matrix/core/models.py
@@ -4,6 +4,23 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    alpha2 = models.CharField(max_length=2, unique=True)
+    alpha3 = models.CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'countries'
+
+    def save(self, *args, **kwargs):
+        self.alpha2 = self.alpha2.upper()
+        self.alpha3 = self.alpha3.upper()
+        super().save(*args, **kwargs)
+
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=100, blank=True)

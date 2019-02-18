049c2345b869e915612afad0f1a2f007f101bc74
Andrius Cepaitis <andrius.c@sanitex.eu>
2019-02-18 15:35:04 +0200

    Add AppServer model

--- matrix/software/admin.py
+++ matrix/software/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 
+from .models import AppServer
+# , SqlServer, Database
 from .models import Project, Program, Deployment
+
+# ==============================================================================
+# Servers & Databases
+# ==============================================================================
+
+@admin.register(AppServer)
+class AppServerAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'ip_address', 'domain_name', 'comment']
+    list_display = ['name', 'ip_address', 'domain_name', 'comment']
+
+# @admin.register(SqlServer)
+# class SqlServerAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'ip_address', 'comment']
+
+# @admin.register(Database)
+# class DatabaseAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'sql_server', 'comment']
+#     save_as = True
 
 # ==============================================================================
 # ProgramAdmin
@@ -23,9 +43,9 @@ class ProgramAdmin(admin.ModelAdmin):
 @admin.register(Deployment)
 class DeploymentAdmin(admin.ModelAdmin):
     search_fields = ['name', 'url']
-    list_display = ['name', 'program', 'environment', 'country', 'url', 'get_project', 'comment']
+    list_display = ['name', 'program', 'environment', 'country', 'url', 'get_project', 'get_app_server_name', 'comment']
     list_display_links = ['name']
-    list_filter = ['program__project', 'environment', 'country', 'program']
+    list_filter = ['program__project', 'environment', 'country', 'app_server', 'program']
     autocomplete_fields = ['program']
     save_as = True
 
@@ -33,6 +53,12 @@ class DeploymentAdmin(admin.ModelAdmin):
         return obj.program.project
     get_project.short_description = 'Project'
     get_project.admin_order_field = 'program__project'
+
+    def get_app_server_name(self, obj):
+        # ??? use global -
+        return obj.app_server.name if obj.app_server is not None else "-"
+    get_app_server_name.short_description = 'App server'
+    get_app_server_name.admin_order_field = 'app_server__name'
 
 # ==============================================================================
 # ProjectAdmin
--- matrix/software/models.py
+++ matrix/software/models.py
@@ -4,6 +4,35 @@ from django.db import models
 import matrix.core.constants as constants
 from matrix.core.models import Country, Contact
 
+# ==============================================================================
+# Servers & Databases
+# ==============================================================================
+
+class ServerBase(models.Model):
+    name = models.CharField(max_length=100)
+    ip_address = models.GenericIPAddressField(protocol='IPv4', blank=True, null=True)
+    domain_name = models.CharField(max_length=100, blank=True)
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, blank=True, null=True)
+    comment = models.CharField(max_length=200, blank=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['name']
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.ip_address)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.upper()
+        super().save(*args, **kwargs)
+
+class AppServer(ServerBase):
+    pass
+
+# ==============================================================================
+# Programs & Deployments
+# ==============================================================================
+
 class Project(models.Model):
     name = models.CharField(max_length=100)
     contact = models.ForeignKey(Contact, on_delete=models.SET_NULL, blank=True, null=True)
@@ -30,14 +59,13 @@ class Program(models.Model):
 class Deployment(models.Model):
     name = models.CharField(max_length=100)
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
-    country = models.ForeignKey(Country, on_delete=models.SET_NULL, blank=True, null=True)
-    url = models.URLField(blank=True)
-
     environment = models.CharField(
         max_length=30, default=constants.ENVIRONMENT_PRODUCTION,
         choices=constants.ENVIRONMENT_CHOICES,
     )
-
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, blank=True, null=True)
+    url = models.URLField(blank=True)
+    app_server = models.ForeignKey(AppServer, on_delete=models.SET_NULL, blank=True, null=True)
     comment = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True)
 
@@ -45,4 +73,4 @@ class Deployment(models.Model):
         return self.name
 
     class Meta:
-        ordering = ['program', 'environment', 'country', 'name']
+        ordering = ['program', 'environment', 'country', 'pk']

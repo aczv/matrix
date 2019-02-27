dbc00865ca1fdd776c8a86e92a2acee602b04afc
Andrius Cepaitis <andrius.c@sanitex.eu>
2019-02-27 15:40:21 +0200

    Add a link from "App servers" page to Deployments page, filtered by app_server__id__exact

--- matrix/software/admin.py
+++ matrix/software/admin.py
@@ -1,8 +1,59 @@
 from django.contrib import admin
 from django.urls import reverse
+from django.utils.translation import gettext as _
+from django.utils.html import format_html
 
 from .models import AppServer, SqlServer, Database
 from .models import Project, Program, Deployment
+
+def admin_changelist_url(model):
+    app_label = model._meta.app_label
+    model_name = model.__name__.lower()
+    return reverse('admin:{}_{}_changelist'.format(
+        app_label,
+        model_name)
+    )
+def admin_changelist_link(
+    attr,
+    short_description,
+    empty_description="-",
+    query_string=None
+):
+    """Decorator used for rendering a link to the list display of
+    a related model in the admin detail page.
+    attr (str):
+        Name of the related field.
+    short_description (str):
+        Field display name.
+    empty_description (str):
+        Value to display if the related field is None.
+    query_string (function):
+        Optional callback for adding a query string to the link.
+        Receives the object and should return a query string.
+    The wrapped method receives the related object and
+    should return the link text.
+    Usage:
+        @admin_changelist_link('credit_card', _('Credit Card'))
+        def credit_card_link(self, credit_card):
+            return credit_card.name
+    """
+    def wrap(func):
+        def field_func(self, obj):
+            related_obj = getattr(obj, attr)
+            if related_obj is None:
+                return empty_description
+            url = admin_changelist_url(related_obj.model)
+            if query_string:
+                url += '?' + query_string(obj)
+            return format_html(
+                '<a href="{}">{}</a>',
+                url,
+                func(self, related_obj)
+            )
+        field_func.short_description = short_description
+        field_func.allow_tags = True
+        return field_func
+    return wrap
 
 # ==============================================================================
 # Servers & Databases
@@ -11,12 +62,28 @@ from .models import Project, Program, Deployment
 @admin.register(AppServer)
 class AppServerAdmin(admin.ModelAdmin):
     search_fields = ['name', 'ip_address', 'domain_name', 'comment']
-    list_display = ['name', 'ip_address', 'domain_name', 'country', 'comment', 'get_deployment_count']
+    list_display = (
+        'name',
+        'ip_address',
+        'domain_name',
+        'country',
+        'comment',
+        'get_deployment_count',
+        'deployments_link',
+    )
     list_select_related = ['country']
 
     def get_deployment_count(self, obj):
         return obj.deployment_set.count()
     get_deployment_count.short_description = 'Deployments'
+
+    @admin_changelist_link(
+        'deployment_set',
+        _('Deployments'),
+        query_string=lambda c: 'app_server__id__exact={}'.format(c.pk)
+    )
+    def deployments_link(self, deployments):
+        return _('Deployments')
 
 @admin.register(SqlServer)
 class SqlServerAdmin(admin.ModelAdmin):
